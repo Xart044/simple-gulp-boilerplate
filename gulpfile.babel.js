c5cac8efbaa610dea59527d7af9c9166620a3c50
Artem Vergun <xhc044x@gmail.com>
2016-10-26 23:44:08 +0300

    upd:enable to minifying img,copying libs into dist,cleaning dist folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,12 +7,14 @@ import rename from 'gulp-rename';
 import minCSS from 'gulp-clean-css';
 import minJS from 'gulp-uglifyjs';
 import htmlmin from 'gulp-htmlmin';
+import imagemin from 'gulp-imagemin';
 import babel from 'gulp-babel';
 import Browsersync from 'browser-sync';
 import browserify from 'browserify';
 import gutil from 'gulp-util';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
+import clean from 'gulp-clean';
 
 gulp.task('sass',()=>{
 	gulp.src('app/sass/main.sass')
@@ -60,6 +62,12 @@ gulp.task('html__minify',()=>{
 	.pipe(gulp.dest('dist/html'));
 });
 
+gulp.task('image__minify',()=>{
+	gulp.src('app/img/**/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/img'));
+});
+
 gulp.task('JS__babel__minify',()=>{
 	gulp.src('app/scripts/*.js')
 	.pipe(babel({
@@ -72,12 +80,22 @@ gulp.task('JS__babel__minify',()=>{
 	.pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('move__libs',()=>{
+	gulp.src('app/libs/**/*')
+	.pipe(gulp.dest('dist/libs'));
+});
+
 gulp.task('dev',['devserver','sass','js'],()=>{
 	gulp.watch('app/sass/**/*.sass',['sass',Browsersync.reload]);
 	gulp.watch('app/js/**/*.js',['js',Browsersync.reload]);
 	gulp.watch('app/index.html',Browsersync.reload);
 });
 
-gulp.task('production',['JS__babel__minify','CSS__prefix__minify','html__minify']);
+gulp.task('cleanDest',()=>{
+	gulp.src('dist')
+	.pipe(clean());
+});
+
+gulp.task('product',['cleanDest','JS__babel__minify','CSS__prefix__minify','html__minify','image__minify','move__libs']);
 
 gulp.task('default',['dev']);
